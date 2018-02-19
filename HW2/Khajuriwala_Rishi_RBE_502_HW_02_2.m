56a19c01972b37f0353a759c55b1b92f70ad4b5e
Rkhajuriwala <rishi.khajuriwala@gmail.com>
2018-02-19 17:26:48 -0500

    2

---
+++ HW2/Khajuriwala_Rishi_RBE_502_HW_02_2.m
@@ -0,0 +1,20 @@
+clc;
+clear all;
+close all;
+syms th(t) x(t) y(t) J m r z1_d(t) z2_d(t)
+t = sym('t','real');
+
+assume(x(t),'real');
+assume(y(t),'real');
+assume(z1_d(t),'real');
+assume(z2_d(t),'real');
+
+z1 = x(t) - (J/m*r)*sin(th(t));
+z2 = y(t) + (J/m*r)*cos(th(t));
+
+z1_dot = diff(z1,t)
+z2_dot = diff(z2,t)
+
+z1_d_dot = diff(z1_dot,t)
+z2_d_dot = diff(z2_dot,t)
+
