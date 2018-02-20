431d8fe64b567b26dd194b79f2d10b4ae32141a2
Rkhajuriwala <rishi.khajuriwala@gmail.com>
2018-02-20 12:55:13 -0500

    added the plot for trajectory before controller

--- HW2/Khajuriwala_Rishi_RBE_502_HW_02_2.m
+++ HW2/Khajuriwala_Rishi_RBE_502_HW_02_2.m
@@ -12,9 +12,9 @@ assume(z2_d(t),'real');
 z1 = x(t) - (J/m*r)*sin(th(t));
 z2 = y(t) + (J/m*r)*cos(th(t));
 
-z1_dot = diff(z1,t)
-z2_dot = diff(z2,t)
+z1_dot = diff(z1,t);
+z2_dot = diff(z2,t);
 
-z1_d_dot = diff(z1_dot,t)
-z2_d_dot = diff(z2_dot,t)
+z1_d_dot = diff(z1_dot,t);
+z2_d_dot = diff(z2_dot,t);
 
