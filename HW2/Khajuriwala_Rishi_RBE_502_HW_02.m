9c84ba54096a03d5c06c82f9f552ace4e7640aa7
Rkhajuriwala <rishi.khajuriwala@gmail.com>
2018-02-20 23:35:57 -0500

    trajectory planner not working

--- HW2/Khajuriwala_Rishi_RBE_502_HW_02.m
+++ HW2/Khajuriwala_Rishi_RBE_502_HW_02.m
@@ -38,12 +38,12 @@ dyi = subs(d_y,t,Ti);
 dyf = subs(d_y,t,Tf);
 
 % compute the jacobian linearization of the vector field.
-% l=1;
-% syms v w theta x y phi l
-% 
-% f= [v*cos(theta); v*sin(theta); (v/l)*tan(phi)];
-% dfdx = jacobian(f,[x;y;theta]);
-% dfdu = jacobian(f,[v;phi]);
+l=1;
+syms v w theta x y phi l
+
+f= [v*cos(theta); v*sin(theta); (v/l)*tan(phi)];
+dfdx = jacobian(f,[x;y;theta]);
+dfdu = jacobian(f,[v;phi]);
 
 % solve linear equations for finding the coefficients in the feasible
 % trajectories.
@@ -52,15 +52,16 @@ dyf = subs(d_y,t,Tf);
 [matA1,matb1] = equationsToMatrix([x0==q0(1), y0==q0(2), dx0*sin(q0(3))-dy0*cos(q0(3))==0, ...
 xi==qi(1), yi==qi(2), dxi*sin(qi(3))-dyi*cos(qi(3))==0],[a(1),a(2),a(3),a(4),b(1),b(2),b(3),b(4)]);
 param1 = matA1\matb1;
-avec1= (param1(1:4)');
-bvec1 = (param1(5:8)');
+avec1= param1(1:4)';
+bvec1 = param1(5:8)';
 
 
 [matA2,matb2] = equationsToMatrix([xi==qi(1), yi==qi(2), dxi*sin(qi(3))-dyi*cos(qi(3))==0, ...
 xf==qf(1), yf==qf(2), dxf*sin(qf(3))-dyf*cos(qf(3))==0],[a(1),a(2),a(3),a(4),b(1),b(2),b(3),b(4)]);
 param2 = matA2\matb2;
-avec2= (param2(1:4)');
-bvec2 = (param2(5:8)');
+avec2= param2(1:4)';
+bvec2 =param2(5:8)';
+
 xdes1 = avec1*basis;
 ydes1 = bvec1*basis;
 % dxdes = avec*dbasis;
@@ -116,10 +117,7 @@ title('Phi Plot');
 % hold on 
 
 % plot(X2(1,:), X2(2,:),'LineWidth', 4);
-% figure;
-% plot(X1(1,:), X1(2,:),'LineWidth', 4);
-% hold on
-% plot(X2(1,:), X2(2,:),'LineWidth', 4);
+
 % [qo1,qo2] = [0,4];
 % [qi1,qi2] = [6,2];
 % [qf1,qf2] = [0,0];
@@ -139,7 +137,7 @@ title('Phi Plot');
 % [qd3,vd3,ad3] = cubic(d3(1),d3(2),d3(3),d3(4),d3(5),d3(6));
 %  t = linspace(0,5,100*5);
 % %% plot-1 Time-position
-% figure;
+% figure(5);
 % % q(1,:) = qd0;
 % % q(1,251:500) = qd1;
 % % q(1,501:750) = qd2;
@@ -148,97 +146,107 @@ title('Phi Plot');
 % title('Trajectory before controller')
 % hold on
 % plot(qd2,qd3,'LineWidth', 4);
-% % hold on
-% % plot(t,qd2,'LineWidth', 4);
-% % hold on
-% % plot(t,qd3,'LineWidth', 4);
-% figure
-% [Xdes1,x1] = ode_tracking(Tf,avec1,bvec1);
-% plot(Xdes1(1,:), Xdes1(2,:), 'LineWidth', 4);
-% % % hold on 
-% title('Trajectory after controller')
-% % title('Trajectory for points (-1,4.5)-->(6,2)-->(0,0)');
-% hold on 
-% [Xdes2,x2] = ode_tracking(Tf,avec2,bvec2);
-% plot(Xdes2(1,:), Xdes2(2,:), 'LineWidth', 4);
-% function [Xdes,X] = ode_tracking(Tf,avec, bvec)
-% % evaluate the desired state.
-% dt=0.01;
-% tsteps=[0:dt:Tf];
-% N=size(tsteps,2);
-% X = zeros(3,N);
-% 
-% % with some initial error
-% % with no-initial error 
-% X(:,1)=[0, 4, pi/6];
-% 
-% Xdes = zeros(3,N);
-% 
-% for i=1:N-1
-%     xvec = X(:,i);
-% x = xvec(1);
-% y = xvec(2);
-% theta = xvec(3);
-% theta= wrapTo2Pi(theta);
-% l=1;
-% t=tsteps(i);
-% basis = [1; t; t^2; t^3];
-% dbasis = [0; 1; 2*t; 3*t^2];
-% ddbasis = [0; 0;2; 6*t];
-% xdes = avec*basis;
-% dxdes = avec*dbasis;
-% ddxdes = avec*ddbasis;
-% 
-% ydes = bvec*basis;
-% dydes = bvec*dbasis;
-% ddydes = bvec*ddbasis;
-% 
-% % compute sin(theta_d)
-% 
-% thetades = atan2(dydes, dxdes);
-% Xdes(:,i)= [xdes;ydes;thetades];
-% 
-% % The desired state.
-% xdes_vec = [xdes; ydes; thetades];
-% 
-% % compute the feedforward in the input.
-% vf = dxdes*cos(thetades) + dydes*sin(thetades);
-% dthetades = 1/vf*(ddydes*cos(thetades) - ddxdes*sin(thetades));
-% wf = dthetades;
-% 
-% 
-% A = [ 0, 0, -vf*sin(thetades);
-%     0, 0,  vf*cos(thetades);
-%     0, 0,             0];
-%  
+% hold on
+% plot(t,qd2,'LineWidth', 4);
+% hold on
+% plot(t,qd3,'LineWidth', 4);
+figure(5);
+[Xdes1,X1] = ode_tracking(Tf,avec1,bvec1);
+plot(Xdes1(1,:), Xdes1(2,:), 'LineWidth', 4);
+% % hold on 
+title('Trajectory after controller')
+% title('Trajectory for points (-1,4.5)-->(6,2)-->(0,0)');
+hold on 
+[Xdes2,X2] = ode_tracking(Tf,avec2,bvec2);
+plot(Xdes2(1,:), Xdes2(2,:), 'LineWidth', 4);
+figure(6);
+plot(X1(1,:), X1(2,:),'LineWidth', 4);
+hold on
+plot(X2(1,:), X2(2,:),'LineWidth', 4);
+function [Xdes,X] = ode_tracking(Tf,avec, bvec)
+% evaluate the desired state.
+dt=0.01;
+tsteps=[0:dt:Tf];
+N=size(tsteps,2);
+X = zeros(3,N);
+
+% with some initial error
+% with no-initial error 
+X(:,1)=[0, 4, 0];
+
+Xdes = zeros(3,N);
+
+for i=1:N-1
+    xvec = X(:,i);
+x = xvec(1);
+y = xvec(2);
+theta = xvec(3);
+theta= wrapTo2Pi(theta);
+l=1;
+t=tsteps(i);
+basis = [1; t; t^2; t^3];
+dbasis = [0; 1; 2*t; 3*t^2];
+ddbasis = [0; 0;2; 6*t];
+xdes = avec*basis;
+dxdes = avec*dbasis;
+ddxdes = avec*ddbasis;
+
+ydes = bvec*basis;
+dydes = bvec*dbasis;
+ddydes = bvec*ddbasis;
+
+% compute sin(theta_d)
+
+thetades = atan2(dydes, dxdes);
+Xdes(:,i)= [xdes;ydes;thetades];
+
+% The desired state.
+xdes_vec = [xdes; ydes; thetades];
+
+% compute the feedforward in the input.
+vf = dxdes*cos(thetades) + dydes*sin(thetades);
+dthetades = 1/vf*(ddydes*cos(thetades) - ddxdes*sin(thetades));
+wf = dthetades;
+deltaf = atan(wf,vf);
+%  A =  [ 0, 0,  vf*cos(thetades);
+%  0, 0, -vf*sin(thetades);
+%  0, 0,             0];
+B = [ sin(thetades),0;
+ cos(thetades),0;
+ tan(deltaf), vf*(tan(deltaf)^2 + 1)]; 
+
+A = [ 0, 0, -vf*sin(thetades);
+    0, 0,  vf*cos(thetades);
+    0, 0,             0];
+ 
 % B = [ cos(thetades), 0;
 %     sin(thetades), 0;
 %     0, 1];
-% 
-% Q= eye(3);
-% R = eye(2);
-% % for all these values of gain the trajectory is not changing
+
+Q= eye(3);
+R = eye(2);
+% for all these values of gain the trajectory is not changing
 % K = [0 0 1;0 0 1];
-% % K = [1 0 0;1 0 0];
-% % K = [1 0 1;1 0 1];
-% % K = [0.1 0.1 0.1;0.1 0.1 0.1];
-% % K = [1 1 1;1 1 1];
-% % K = [10 10 10;10 10 10];
-% % K = [0 0 0;0 0 0];
-% % K= lqr(A,B,Q,R);
-% u = -K*(xvec - xdes_vec) + [vf; wf];
-% dxvec = [u(1)*cos(theta);u(1)*sin(theta);u(2)];
-% X(:,i+1)= dxvec*dt+ X(:,i);
-% end
-% for i=1:N;
-%     t=tsteps(i);
-%     basis = [1; t; t^2; t^3];
-% dbasis = [0; 1; 2*t; 3*t^2];
-% ddbasis = [0; 0;2; 6*t];
-% Xdes(1,i) = avec*basis;
-%     Xdes(2,i)= bvec*basis;
-% end
-% end
+% K = [1 0 0;1 0 0];
+% K = [1 0 1;1 0 1];
+% K = [0.1 0.1 0.1;0.1 0.1 0.1];
+% K = [1 1 1;1 1 1];
+% K = [10 10 10;10 10 10];
+% K = [0 0 0;0 0 0];
+K= lqr(A,B,Q,R);
+u = -K*(xvec - xdes_vec) + [vf; wf];
+dxvec = [u(1)*cos(theta);u(1)*sin(theta);u(2)];
+X(:,i+1)= dxvec*dt+ X(:,i);
+end
+for i=1:N;
+    t=tsteps(i);
+    basis = [1; t; t^2; t^3];
+dbasis = [0; 1; 2*t; 3*t^2];
+ddbasis = [0; 0;2; 6*t];
+Xdes(1,i) = avec*basis;
+    Xdes(2,i)= bvec*basis;
+end
+end
 %  function [qd,vd,ad] =cubic(qi, qf, vi,vf,ti,tf)
 % 
 % t = linspace(ti,tf,100*(tf-ti));
